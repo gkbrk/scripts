5aebecb5465b4be6d378805499e9f1116b116808
Gokberk Yaltirakli <git@gkbrk.com>
2023-05-27 18:05:59 +0300

    Update firefox clean script

--- clean-firefox.sh
+++ clean-firefox.sh
@@ -5,34 +5,19 @@ log () {
     printf "[%s] %s\n" "${DATE}" "${1}"
 }
 
-vacuumSqlite () {
+sqliteRun () {
     file="${1}";shift
-    log "Vaccuming ${file}..."
-    sqlite3 "${file}" "REINDEX"
-    sqlite3 "${file}" "VACUUM"
-    sqlite3 "${file}" "REINDEX"
+    qry="${1}";shift
+    sqlite3 "${file}" "${qry}"
 }
 
-log "Reading profiles..."
-
-find "${HOME}/.mozilla/firefox" -maxdepth 1 -type d |\
-    while IFS= read -r profile
+find "${HOME}/.mozilla/firefox" -type f -wholename '*.sqlite' |\
+    while IFS= read -r file
     do
-        log "Processing ${profile}..."
-
-        find "${profile}" -type f -wholename '*.sqlite' |\
-            while IFS= read -r db
-            do
-                vacuumSqlite "${db}"
-            done
+        log "Processing ${file}..."
+        sqliteRun "${file}" "REINDEX"
+        sqliteRun "${file}" "VACUUM"
+        sqliteRun "${file}" "ANALYZE"
+        sqliteRun "${file}" "PRAGMA OPTIMIZE"
+        sqliteRun "${file}" "REINDEX"
     done
-
-#timevalue=`python3 -c "import time;print(round((time.time() - 24*60*60*30) * 1000000))"`
-
-# Clean old cookies
-#echo "Cleaning old cookies..."
-#sqlite3 ~/.mozilla/firefox/$profile/cookies.sqlite "DELETE FROM moz_cookies WHERE lastAccessed < $timevalue;"
-
-# Clean history
-#echo "Cleaning history..."
-#sqlite3 ~/.mozilla/firefox/$profile/places.sqlite "DELETE FROM moz_places WHERE last_visit_date < $timevalue;"
